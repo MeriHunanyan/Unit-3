964ae168498816cf50ffdacfafc29562042aac51
hunanyam_wlhs <hunanyam@wlhs.wlwv.k12.or.us>
2024-10-15 15:01:13 -0700

    jehbg

--- src/math_class/Main.java
+++ src/math_class/Main.java
@@ -16,18 +16,27 @@ public class Main {
         // ClassName.methodName(); e.g. Math.random()
         // ClassName.staticVariableName(); e.g. Math.PI
 
+        double myPI = Math.PI;
+
         // abs
-
+        int num1 = -2;
+        int absValue = Math.abs(num1);//If you want whole number you have to cast is as an integer
+        System.out.println(Math.abs(num1));
         // pow
-
+        double num2 = Math.pow(3, 5);
+        System.out.println(num2);
         // sqrt
-
+        double num3 = Math.sqrt(80);
+        System.out.println(num3);
+        System.out.println(Math.pow(num3,2));
 
 
         //create a random number in the range [0,1)
-
+        double random1 = Math.random();
+        System.out.println(random1);
         //create a random number in the range [0, n)
-
+        //example: [0, 6)
+        double random2 = Math.random();
         // create a random number in the range [0,n]
 
         // create a random number in the range [m,n]
